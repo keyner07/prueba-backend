0d1dffb3673bbc0ac0e412f4184b0b297883f0e0
Keyner Baez <skerling19@hotmail.com>
2021-03-18 18:15:12 -0400

    add the export to the root file

---
+++ src/entity/index.ts
@@ -0,0 +1,7 @@
+
+/**
+ * Export of the entities
+ */
+export { Content as ContentAbstractEntity} from "./Content.entity";
+export { Dealer as DealerEntity} from "./Dealer.entity";
+export { User as UserEntity } from "./User.entity";
