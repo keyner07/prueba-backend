a03e00638c10c0ff1286d229dc9d125727c91ee6
Keyner Baez <skerling19@hotmail.com>
2021-03-18 18:14:42 -0400

    create dealer entity

---
+++ src/entity/Dealer.entity.ts
@@ -0,0 +1,21 @@
+import { Entity, Column } from 'typeorm';
+import { ContentAbstractEntity} from ".";
+
+/**
+ * Class Dealer
+ */
+@Entity()
+export class Dealer extends ContentAbstractEntity {
+
+    @Column({ unique: true })
+    name!: string;
+
+    @Column()
+    address!: string;
+
+    constructor(dealer: Partial<Dealer>) {
+        super(dealer);
+        Object.assign(this, dealer);
+    }
+}
+
