57f0fbcd4a3ee61a0688943e63a3c7895a512d53
Keyner Baez <skerling19@hotmail.com>
2021-03-18 18:14:16 -0400

    create abstract class content with common fields

---
+++ src/entity/Content.entity.ts
@@ -0,0 +1,20 @@
+import {Column, CreateDateColumn, PrimaryGeneratedColumn} from 'typeorm';
+
+
+/**
+ * Abstract class Content
+ */
+export abstract class Content {
+    @PrimaryGeneratedColumn('increment')
+    id!: number;
+
+    @Column('boolean', { default: true })
+    isActive!: boolean;
+
+    @CreateDateColumn()
+    createdAt!: Date;
+
+    constructor(content: Partial<Content>) {
+        Object.assign(this, content);
+    }
+}
