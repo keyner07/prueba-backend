7b22cd4cab800cfd359ae58b50e6fbb0918e4f96
Keyner Baez <skerling19@hotmail.com>
2021-03-18 18:35:45 -0400

    add the export to the repository

--- src/entity/index.ts
+++ src/entity/index.ts
@@ -1,6 +1,11 @@
 /**
- * Export of the entities
+ * Export all the entities
  */
 export { Content as ContentAbstractEntity } from './Content.entity';
 export { Dealer as DealerEntity } from './Dealer.entity';
 export { User as UserEntity } from './User.entity';
+
+/**
+ * Export the repository
+ */
+export { EntityRepository } from './EntityRepository';
