03e9a4247ccc28c0ea71ce81e87d8b45faeebb54
Keyner Baez <skerling19@hotmail.com>
2021-03-18 19:05:26 -0400

    add the comments to the entity repository

--- src/entity/EntityRepository.ts
+++ src/entity/EntityRepository.ts
@@ -8,26 +8,54 @@ import {
     UpdateResult,
 } from 'typeorm';
 import { QueryDeepPartialEntity } from 'typeorm/query-builder/QueryPartialEntity';
-
+/**
+ * Class entity repository with method for manipulate the db
+ */
 export class EntityRepository<T> {
     private entity: Repository<T>;
 
+    /**
+     * Constructor of EntityRepository
+     * @param {EntityTarget<T>} entity Given entity class.
+     */
     constructor(entity: EntityTarget<T>) {
         this.entity = getRepository(entity);
     }
 
+    /**
+     * Saves a given entity in the database.
+     * @param {T} criteria Given criteria to save.
+     * @returns {Promise<T>}
+     */
     async create(criteria: T): Promise<T> {
         return await this.entity.save(criteria);
     }
 
+    /**
+     * Finds first entity that matches given conditions.
+     * @param {T} criteria Given criteria to findOne.
+     * @param {FindOneOptions<T>} options criteria to find specific entity.
+     * @returns {Promise<T | undefined>}
+     */
     async findOne(criteria: T, options?: FindOneOptions<T>): Promise<T | undefined> {
         return await this.entity.findOne(criteria, options);
     }
 
+    /**
+     * Finds entities that match given options.
+     * @param {FindManyOptions<T>} options Criteria to find specific entities.
+     * @returns {Promise<T[] | undefined>}
+     */
     async find(options?: FindManyOptions<T>): Promise<T[] | undefined> {
         return await this.entity.find(options);
     }
 
+    /**
+     * Updates entity partially that matches given conditions.
+     * @param {FindConditions<T> | number} criteria Given criteria for find.
+     * @param {QueryDeepPartialEntity<T>} partialEntity
+     * @returns {Promise<UpdateResult>}
+     */
     async update(
         criteria: FindConditions<T> | number,
         partialEntity: QueryDeepPartialEntity<T>,
