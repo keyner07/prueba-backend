a9ce6aac553e9b6d09ace0c0516d1dbe141b52f8
Keyner Baez <skerling19@hotmail.com>
2021-03-19 09:31:37 -0400

    environment variables

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,8 +5,6 @@ dotenv.config();
 export default abstract class Config {
     private static readonly ENV: NodeJS.ProcessEnv = process.env;
     public static readonly PORT: number = parseInt(Config.ENV.PORT!) || 3000;
-    public static readonly SALT: number = parseInt(Config.ENV.SALT!);
+    public static readonly SALT: number = parseInt(Config.ENV.SALT!) || 10;
     public static readonly JWTSECRET: string = Config.ENV.JWTSECRET || 'somesecret';
-    public static readonly HOSTSMTP: string = Config.ENV.HOSTSMTP || 'smtp.mailtrap.io';
-    public static readonly FROMSMTP: string = Config.ENV.FROMSMTP || 'from@example.com';
 }
